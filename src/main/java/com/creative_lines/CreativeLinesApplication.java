843af14cae9cec18b5a26c70da70976811075132
shahriar <asraf@exabyting.com>
2025-11-26 22:20:44 +0600

    first commit

---
+++ src/main/java/com/creative_lines/CreativeLinesApplication.java
@@ -0,0 +1,13 @@
+package com.creative_lines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativeLinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativeLinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creative_lines/CreativeLinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creative_lines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativeLinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
